0888ecc2d332b5a6d99fcd91ef771708296dec09
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-01-19 02:52:58 +0300

    gerekli düzenlemeler yapıldı

--- Kampintro/Program.cs
+++ Kampintro/Program.cs
@@ -6,8 +6,51 @@ namespace Kampintro
     {
         static void Main(string[] args)
         {
-           
-         
+
+            Product product1 = new Product();
+            product1.ProductName = "Paslanmaz Çelik";
+            product1.ProductWeight = 10;
+            product1.ProductPrice = 1000;
+
+            Product product2 = new Product();
+            product2.ProductName = "Dökme Demir";
+            product2.ProductWeight = 20;
+            product2.ProductPrice = 1500;
+
+            Product product3 = new Product();
+            product3.ProductName = "Bakır";
+            product3.ProductWeight = 30;
+            product3.ProductPrice = 2000;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+            Console.WriteLine("\n for ---");
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].ProductName + ":" + products[i].ProductWeight + ":" + products[i].ProductPrice);
+            }
+
+
+
+            Console.WriteLine("\n foreach ---");
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductName + ":" + product.ProductWeight + ":" + product.ProductPrice);
+            }
+            Console.WriteLine("\n while ---");
+            int sayac = 0;
+            while (sayac < products.Length)
+            {
+                Console.WriteLine(products[sayac].ProductName + ":" + products[sayac].ProductWeight + ":" + products[sayac].ProductPrice);
+                sayac++;
+            }
+        }
+
+
+        class Product
+        {
+            public string ProductName { get; set; }
+            public int ProductWeight { get; set; }
+            public int ProductPrice { get; set; }
 
 
 
---
+++ ternaryOperatoru/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ternaryOperatoru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool medeniHal = true;
+            string mesaj = medeniHal == true ? "Evlilere kampanya..." : "Bekarlara kampanya...";
+            Console.WriteLine(mesaj);         
+        }
+    }
+}
