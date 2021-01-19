49fb50a6237c83198aed10a28fe9f3cc534a6e6f
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-01-19 03:32:54 +0300

    switch

---
+++ switchOperatoru/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace switchOperatoru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            switch (x)
+            {
+
+                case 3:
+                    Console.WriteLine("değeriniz 3");
+                    break;
+                case 13:
+                    Console.WriteLine("değeriniz 13");
+                    break;
+                case 10:
+                    Console.WriteLine("değeriniz 10");
+                    break;
+            }
+        }
+    }
+}
