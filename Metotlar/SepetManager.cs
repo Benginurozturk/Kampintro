59114adcbe421ce749950f86e018392f15f47410
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-01-20 21:03:26 +0300

    ref and out

--- Metotlar/SepetManager.cs
+++ Metotlar/SepetManager.cs
@@ -6,5 +6,9 @@ namespace Metotlar
 {
     class SepetManager
     {
+        public void Ekle()
+        { 
+           Console.WriteLine("Sepete eklendi");
+        }
     }
 }
---
+++ ref and out/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ref_and_out
+{
+    class Program
+    {
+       
+         static void Main()
+            {
+
+                int sayimizOut;
+                DegistirOut(out sayimizOut);
+                Console.WriteLine(sayimizOut);
+
+                int sayimizRef = 0;  // değer verilmediğinde hata verecektir.
+                DegistirRef(ref sayimizRef);
+                Console.WriteLine(sayimizRef);
+            }
+
+            static void DegistirRef(ref int sayiRef)
+            {
+                sayiRef = 1234;
+            }
+
+            static void DegistirOut(out int sayiOut)
+            {
+                sayiOut = 1234;
+            }
+        }
+    }
+    
+
