73aa47cf4244bed67e7bb2a17efe8960b70a7401
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-02-06 18:13:32 +0300

    ----

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
