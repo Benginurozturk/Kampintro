7a56befe3940f0c4cc562404098ab35b02f87516
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-02-08 02:54:51 +0300

    SQL Odev

---
+++ SQLQuery3.sql
@@ -0,0 +1,4 @@
+﻿select p.ProductName [Ürün Adı],Sum(od.UnitPrice*od.Quantity) [Kazanılan Toplam Miktar] from
+Products p inner join [Order Details] od on p.ProductID=od.ProductID
+inner join Orders o on od.OrderID=o.OrderID
+group by p.ProductName
